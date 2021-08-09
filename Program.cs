6099980901552850d7241d657d407d497b9eba22
GiovannaMarques <giovannalimamarques@hotmail.com>
2021-08-09 19:41:54 -0300

    add repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aulascsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
