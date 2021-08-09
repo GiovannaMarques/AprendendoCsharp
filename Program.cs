2710280a754802e970e3e6fadbc223ac59d9f835
GiovannaMarques <giovannalimamarques@hotmail.com>
2021-08-09 19:48:33 -0300

    add tipos de dados

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace aulascsharp
+namespace Documentoscode
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Qual é o seu nome de usuário? ");
+
+            
+            string nome = Console.ReadLine();
+
+            int idade = 21;
+
+            double altura = 1.64;
+
+            float peso = 62;
+
+            bool choveu = true;
+
+
+            Console.WriteLine($" Seu nome é {nome}, sua idade é {idade}, sua altura é {altura}, seu peso é {peso}, e o tempo {choveu}");
+
+
+
+
+
+                    }
     }
 }
